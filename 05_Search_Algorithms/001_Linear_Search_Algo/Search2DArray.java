36c767a03894604653519d2b13b0db6ec6024df0
Sian Rodrigues <sianrods250@gmail.com>
2024-09-04 20:27:32 +0530

    txt file

---
+++ 05_Search_Algorithms/001_Linear_Search_Algo/Search2DArray.java
@@ -0,0 +1,42 @@
+// It is important to note that all 2D arrays may be symmectric hence concept of jagged arrays becomes very crucial 
+//  jagged arrays and how to find the length on inner array
+import java.util.Arrays;;
+public class Search2DArray {
+    public static void main(String args[]) {
+        int numarr[][] = {
+                { 34, 3, 5, 46, 5, 6 },
+                { 243, 4, 5, 4, 32, -23, -4, -21 },
+                { 234, 3, 5, 457, 584 }
+        };
+        System.out.println(Arrays.toString(searchIndexArray(numarr, -23)));
+
+    }
+
+// As there are two indices involved the dtype of the return array will be an array itself 
+// Note that here while we return the value we have to create and return an object using the 'new' keyword of type int
+
+    static int[] searchIndexArray(int arr[][],int ele ){
+        // Checking the boundary conditions
+        if(arr.length==0){
+            return new int []{-1,-1} ; 
+        }
+
+        else{
+            for(int rows=0;rows<arr.length;rows++){
+                for(int cols=0;cols<arr[rows].length;cols++){
+                    if(arr[rows][cols]==ele){
+                        return new int[]{rows,cols};
+                    }
+                }
+            }
+        }
+
+        // If no index if found 
+        return new int[]{-1,-1};
+    }
+
+
+    // Finding the Maximum and minimum in 2D Array 
+    
+}
+
--- 05_Search_Algorithms/001_Linear_Search_Algo/SearchMinMax.java
+++ 05_Search_Algorithms/001_Linear_Search_Algo/SearchMinMax.java
@@ -1,5 +1,40 @@
 public class SearchMinMax {
-    public static void main(String args[]){
-        
+    public static void main(String args[]) {
+        int nums[] = { 134, 32, 432, 3435, 3432454, 34, 32, 35 };
+        System.out.println(minArray(nums));
+        System.out.println(maxArray(nums));
     }
+
+    static int minArray(int arr[]) {
+        // Boundary Conditions
+        if (arr.length == 0) {
+            return Integer.MAX_VALUE;
+        }
+
+        // Assuming the element at the first position to be maximum
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    static int maxArray(int arr[]) {
+        if (arr.length == 0) {
+            return Integer.MAX_VALUE;
+        }
+
+        // Assuming the element at the first position to be maximum
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+
+    }
+
 }
