812df8ec3817ba82479668e893d7282ed6be7164
Sian Rodrigues <sianrods250@gmail.com>
2024-12-31 12:19:29 +0530

    files changed

--- 05_Search_Algorithms/002_Binary_Search_Algo/PracticeProblems/FirstAndLastPosition.java
+++ 05_Search_Algorithms/002_Binary_Search_Algo/PracticeProblems/FirstAndLastPosition.java
@@ -1,55 +1,102 @@
+// We can find the Floor and the Ceiling Index for a given target and then subtract them to compare existence of the 
+// element in the given numsay
+
+
 public class FirstAndLastPosition {
-    public static void main(String args[]) {
-        // Only static methods can invoke other static methods if we don't want to
-        // create seperate classes
-        // Nad want to implement same under single class
-        int arr[] = { 2, 5, 5, 5, 7 };
+    public static void main(String args[]){
+        int nums[] = {};
         // target = 8
-        int[] solution = searchRange(arr, 5);
+        int[] solution = solution(nums, 2);
         System.out.println("The First and End position of the target variable is : " + solution[0] + solution[1]);
+
     }
 
-    static int[] searchRange(int[] nums, int target) {
-        // Using time complexity of O(log n )
-        int start = 0;
-        int end = nums.length - 1;
-        int middle = (start + end) / 2;
-        int solution[] = new int[2];
-        int count = 0;
 
-        // Given that the order is in Ascending Format
-        // 3 5 5 5 7
-        // middle element first occurrence at second position
-        // It's wrong to assume that the first occurence of the given target will be at
-        // 1st middle
 
-        do {
-            if (nums[middle] == target) {
-                if (count == 0) {
-                    solution[0] = middle;
-                    start = middle + 1;
-                    middle = (start + end) / 2;
-                    count++;
-                } else {
-                    count++;
-                    start = middle + 1;
-                    middle = (start + end) / 2;
 
-                }
-            } else if (nums[middle] < target) {
-                start = middle + 1;
-                middle = (start + end) / 2;
+    static int ceilIndex(int nums [],int target){
+        // Assuming Acsending Order 
+        int start=0;
+        int end = nums.length-1;
+        int middle =(start+end)/2;
 
-            } else if (nums[middle] > target) {
-                end = middle - 1;
-                middle = (start + end) / 2;
+        do{
+            if(nums[middle]==target){
+                start=middle+1;
+                middle=(start+end)/2;
+
+            }else if (nums[middle]>target){
+                end=middle-1;
+                middle=(start+end)/2;
+
+            }else if (nums[middle]<target){
+                start=middle+1;
+                middle=(start+end)/2;
+                
 
             }
-        } while (!(start > end));
+        }while(!(start>end));
+        return start;
 
-        solution[1] = solution[0] + count;
-
-        return solution;
 
     }
+
+    static int floorIndex(int nums [],int target){
+        // Assuming Acsending Order 
+        int start=0;
+        int end = nums.length-1;
+        int middle =(start+end)/2;
+
+        do{
+            if(nums[middle]==target){
+                end=middle-1;
+                middle=(start+end)/2;
+
+            }else if (nums[middle]>target){
+                end=middle-1;
+                middle=(start+end)/2;
+
+            }else if (nums[middle]<target){
+                start=middle+1;
+                middle=(start+end)/2;
+                
+
+            }
+        }while(!(start>end));
+        return end;
+
+    }
+
+    static int[] solution(int nums[],int target){
+    int sol[] = new int[2];
+    if (nums.length>0){
+        int ceil=ceilIndex(nums, target);
+        int  floor = floorIndex(nums, target);
+       if(ceil-floor>1){
+        sol[0]=floor+1;
+        sol[1]=ceil-1;
+        return sol;
+       }else if (ceil-floor==1){
+        sol[0]=-1;
+        sol[1]=-1;
+        return sol;
+
+       }
+       else {
+        sol[0]=-1;
+        sol[1]=-1;
+        return sol;
+       }
+    }else {
+        sol[0]=-1;
+        sol[1]=-1;
+        return sol;
+        
+    }
+
+
+    } 
+
+
+
 }
