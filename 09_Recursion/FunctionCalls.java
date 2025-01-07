a03633e00478595340f2d9cc6eb0468316fd8ed9
Sian Rodrigues <sianrods250@gmail.com>
2025-01-07 12:41:02 +0530

    .

--- 09_Recursion/FunctionCalls.java
+++ 09_Recursion/FunctionCalls.java
@@ -1,5 +1,27 @@
 
-
 public class FunctionCalls {
-    
+    public static void main(String[] args) {
+        printNumberTopDown(10);
+        printNumberBottomUp(10);
+    }
+
+    static void printNumberTopDown(int num) {
+        if (num == 0) {
+            System.out.println(num);
+            return;
+        } else {
+            System.out.println(num);
+            printNumberTopDown(num - 1);
+        }
+    }
+
+    static void printNumberBottomUp(int num) {
+        if(num==30){
+            System.out.println(num);
+            return;
+        }else{
+            System.out.println(num);
+            printNumberBottomUp(num+1);
+        }
+    }
 }
