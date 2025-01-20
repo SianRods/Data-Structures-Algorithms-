f52dbca5a5756ebdacabd523324729e91afbbfcc
Sian Rodrigues <sianrods250@gmail.com>
2025-01-20 23:27:15 +0530

    daily code

--- Leetcode Problems/ContainsDuplicates.java
+++ Leetcode Problems/ContainsDuplicates.java
@@ -1,5 +1,19 @@
-public class ContainsDuplicates {
-    static boolean checkDuplicate(int arr[]) {
+import java.util.Arrays;
 
+public class ContainsDuplicates {
+
+    public static void main(String args[]) {
+        int arr[] = { 1, 2, 3, 1 };
+        System.out.println(checkDuplicate(arr));
+    }
+
+    static boolean checkDuplicate(int arr[]) {
+        Arrays.sort(arr);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] == arr[i]) {
+                return true;
+            }
+        }
+        return false;
     }
 }
