718919f83f29764c29745785240d065b783d4744
Sian Rodrigues <sianrods250@gmail.com>
2025-02-13 22:47:52 +0530

    selection updated

--- 06_Sorting_Algorithms/Selection Sort/SelectionSortMyCode.java
+++ 06_Sorting_Algorithms/Selection Sort/SelectionSortMyCode.java
@@ -8,35 +8,23 @@ import java.util.Arrays;
 
 public class SelectionSortMyCode {
     public static void main(String args[]) {
-        int arr[] = { -5, -4,0, -3,- 2, -1 };
+        int arr[] = { -5, -4, 0, -3, -2, -1 };
         System.out.println(Arrays.toString(arr));
         selectionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     static void selectionSort(int arr[]) {
-        // using the Loop
-
-        for (int i = 0; i < arr.length; i++) {
-            // Checking the maximum element out of the array and inserting it at the right
-            // postion
-            int max = 0;
-            int index = 0;
-            for (int j = 1; j < arr.length - i; j++) {
-                max = arr[0];
-                if (arr[j] > max) {
-                    max = arr[j];
-                    index = j;  
-                } else {
-                    continue;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
             }
-            // After the loop condtion swapping the given maximum element at the desired
-            // position
-            int temp = arr[arr.length - i - 1];
-            arr[arr.length - i - 1] = arr[index];
-            arr[index] = temp;
-
         }
     }
 }
